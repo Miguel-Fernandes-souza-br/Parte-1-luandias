84926c54ec7d61a557090276e1780fbb7b3b5fdd
Miguel <miguel_f_de-souza@estudante.sesisenai.org.br>
2026-07-21 14:39:51 -0300

    Adicionado parte 2

--- parte 2.js
+++ parte 2.js
@@ -1,47 +1,3 @@
-const alunos = [
-  { nome: "Arthur", notas: [8, 9, 7] },
-  { nome: "João", notas: [3, 4, 5] },
-  { nome: "Roberto", notas: [6, 7] }
-];
-
-
-function buscarAluno(nome) {
-  for (let i = 0; i < alunos.length; i++) {
-    if (alunos[i].nome.toLowerCase() === nome.toLowerCase()) {
-      return alunos[i];
-    }
-  }
-  return undefined;
-}
-
-console.log(buscarAluno("Luan"))
-
-
-function calcularMedia(aluno) {
-  if (!aluno.notas || aluno.notas.length === 0) {
-    return 0;
-  }
-  
-  let soma = 0;
-  for (let i = 0; i < aluno.notas.length; i++) {
-    soma += aluno.notas[i];
-  }
-  
-  return soma / aluno.notas.length;
-}
-
-
-function situacao(media) {
-  if (media >= 6) {
-    return "APROVADO";
-  } else if (media >= 4) {
-    return "RECUPERACAO";
-  } else {
-    return "REPROVADO";
-  }
-}
-
-
 function cadastrarAluno() {
   let nome = prompt("Digite o nome do aluno que deseja cadastrar:");
   
@@ -88,3 +44,22 @@ function removerAluno() {
   alert("Aluno " + nome + " removido com sucesso!");
 }
 
+function lancarNota() {
+  let nome = prompt("Digite o nome do aluno:");
+  let aluno = alunos.find(a => a.nome === nome);
+  
+  if (!aluno) {
+    alert("Aluno não encontrado!");
+    return;
+  }
+  
+  let nota = Number(prompt("Digite a nota (0 a 10):"));
+  
+  if (nota < 0 || nota > 10 || isNaN(nota)) {
+    alert("Nota inválida!");
+    return;
+  }
+  
+  aluno.notas.push(nota);
+  alert("Nota lançada com sucesso!");
+}
